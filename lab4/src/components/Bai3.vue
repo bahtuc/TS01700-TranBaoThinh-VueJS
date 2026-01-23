823720726e1fc8c07c00800b13d1dd6939e52015
BachNone <tranbaothinh00@gmail.com>
2026-01-23 20:09:11 +0700

    Hoàn thành Lab 4

---
+++ lab4/src/components/Bai3.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>{{ greeting }}</h1>
+    <button v-bind:title="buttonTitle" @click="chageGreeting">Click để thay đổi
+    </button>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            greeting: 'Xin chào bạn!',
+            buttonTitle: 'Nhấp để thay đổi lời chào'
+        };
+    },
+    methods: {
+        chageGreeting() {
+            this.greeting = 'Hello Wold!';
+            this.buttonTitle = 'Lời chào đã cập nhật';
+
+        }
+    },
+}
+</script>
---
+++ lab4/src/components/Hello.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Chào mừng đến với VuesJS</h1>
+</template>
+<style>
+    h1{color: red;}
+</style>
