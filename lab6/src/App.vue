7439dc84fc91e2500cd1a0705700a06c46189e93
BachNone <tranbaothinh00@gmail.com>
2026-01-31 00:21:43 +0700

    hoàn thành lab6

---
+++ lab6/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import bai1 from './components/bai1.vue';
+import bai2 from './components/bai2.vue';
+import bai3 from './components/bai3.vue';
+import bai4 from './components/bai4.vue';
+</script>
+
+<template>
+  <div class="container">
+    <hr>
+    <h1>bai1</h1>
+    <bai1 />
+    <hr>
+    <h1>Bài 2</h1>
+    <bai2 />
+    <hr>
+    <h1>Bai 3</h1>
+    <bai3 />
+    <hr>
+    <h1>Bai 4</h1>
+    <bai4 />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ lab6/src/components/bai2.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+const month = ref("1");
+</script>
+<template>
+    <div class="container">
+        <h3>Nhập tháng của bạn :</h3>
+        <input type="number" v-model.number="month" placeholder="Nhập tháng">
+        <p v-if="month < 1 || month > 12" style="color: red;">Vui lòng nhập từ tháng 1 đến tháng 12</p>
+        <p v-else-if="month <= 3">Mùa xuân</p>
+        <p v-else-if="month > 3 && month <= 6">Mùa hạ</p>
+        <p v-else-if="month > 6 && month <= 9">Mùa thu</p>
+        <p v-else>Mùa đông</p>
+    </div>
+
+</template>
